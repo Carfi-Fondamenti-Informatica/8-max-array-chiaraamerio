c19c15a111f225df074b9fc1f103c51e73d3b91c
chiaraamerio <100305899+chiaraamerio@users.noreply.github.com>
2022-05-02 16:30:27 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,6 +1,5 @@
 #include"lib.h"
-
-double funzione(double valori[], int d){
+float funzione(float valori[], int d){
     float maggiore=0;
     for(int i=0; i<d; i++){
         if(maggiore<valori[i]){
