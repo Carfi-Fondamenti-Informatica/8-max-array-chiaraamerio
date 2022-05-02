3cbfc986d8cecf8cc694c27b1fbb14dcb8df2757
chiaraamerio <100305899+chiaraamerio@users.noreply.github.com>
2022-05-02 16:17:35 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,10 @@
+#include"lib.h"
 
+float funzione(float valori[], int d){
+    float maggiore=0;
+    for(int i=0; i<d; i++){
+        if(maggiore<valori[i]){
+           maggiore=valori[i];
+        }
+    } return maggiore;
+}
