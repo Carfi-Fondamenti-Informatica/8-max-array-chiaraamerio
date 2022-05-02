552c5726d29d433b217a51ba60c0cb3fb12dd7d4
chiaraamerio <100305899+chiaraamerio@users.noreply.github.com>
2022-05-02 16:19:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
 
 int main(){
+   int d=0;
+     cout<<"inserire la dimensione dell'array"<<endl;
+     cin>>d;
+     float valori[d];
+     cout<<"inserire i valori degli array"<<endl;
+     for (int i=0; i<d; i++){
+         cin>>valori[i];
+     }
+     float x= funzione(valori,d);
+     cout<<x<<endl;
   return 0;
 }
